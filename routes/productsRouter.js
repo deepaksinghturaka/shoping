e49f7ed20f673044e87f4738177e60092709e684
deepaksinghturaka <devdeepak394@gmail.com>
2025-01-06 22:38:36 +0530

    Update productsRouter.js

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const router = express.Router();
 const upload = require("../config/multer-config");
 const productModel = require("../models/prodect-model");
